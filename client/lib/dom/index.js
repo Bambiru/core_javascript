95d7372edbcee925ef01896f89378d06ab5c58c6
bambiru <dj70083798@gmail.com>
2023-12-18 15:13:49 +0900

    [learn] module - 모듈 프로그래밍 학습

---
+++ client/lib/dom/index.js
@@ -0,0 +1,7 @@
+/* dom re-export */
+
+export * from './css.js';
+export * from './getNode.js';
+export * from './insert.js';
+export { default as clearContents } from './clear.js';
+export { default as bindEvent } from './bindEvent.js';
--- client/main.js
+++ client/main.js
@@ -1,3 +1,27 @@
-const a = 'hello';
+/* 모듈 프로그래밍 */
+// name export (이름 내보내기)=> {이름}
+// default export (기본 내보내기)=> 이름
+// import { getNode } from './lib/dom/getNode.js';
+// import { insertLast } from './lib/dom/insert.js';
+// import clearContents from './lib/dom/clear.js';
+import { getNode, clearContents, insertLast } from './lib/index.js';
 
-console.log(navigator.language);
+const first = getNode('#firstNumber');
+const second = getNode('#secondNumber');
+const result = getNode('.result');
+
+//1. input value 값 가져오기
+
+function handleInput() {
+  const firstValue = +first.value;
+  const secondValue = +second.value;
+  const total = firstValue + secondValue;
+
+  clearContents(result);
+  insertLast(result, total);
+}
+
+first.addEventListener('input', handleInput);
+second.addEventListener('input', handleInput);
+//2. 두 수의 합 더하기
+//3. 합계 랜더링 하기
