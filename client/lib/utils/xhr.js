3dabb986fce3eefb4aaca8a009e71f6afe7e2a08
bambiru <dj70083798@gmail.com>
2023-12-21 08:55:29 +0900

    [learn] xhr - callback 방식을 활용한 비동기 통신 - 함수의 메서드 지정

--- client/lib/utils/xhr.js
+++ client/lib/utils/xhr.js
@@ -10,16 +10,27 @@
 
 */
 
+/* -------------------------------------------------------------------------- */
+/*                                XHR callback                                */
+/* -------------------------------------------------------------------------- */
+
 function xhr({
   method = 'GET',
   url = '',
   onSuccess = null,
   onFail = null,
+  body = null,
+  headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*', // 권한 요청,
+  },
 } = {}) {
   const xhr = new XMLHttpRequest();
 
   xhr.open(method, url);
-
+  Object.entries(headers).forEach(([key, value]) => {
+    xhr.setRequestHeader(key, value);
+  });
   /* state가 바뀔때마다 호출이 되는 이벤트이다. */
   xhr.addEventListener('readystatechange', () => {
     const { readyState, status, response } = xhr;
@@ -36,14 +47,101 @@ function xhr({
     }
   });
 
-  xhr.send(); // POST 통신 시 필요하다.
+  xhr.send(JSON.stringify(body)); // POST 통신 시 필요하다.
 }
-xhr({
-  url: 'https://jsonplaceholder.typicode.com/users',
-  onSuccess(data) {
-    console.log(data);
+// xhr({
+//   method: 'POST',
+//   url: 'https://jsonplaceholder.typicode.com/users',
+//   onSuccess(data) {
+//     console.log(data);
+//   },
+//   onFail({ message }) {
+//     console.log(message);
+//   },
+//   body: { name: 'tiger' },
+// });
+
+xhr.get = (url, onSuccess, onFail) => xhr({ url, onSuccess, onFail });
+
+xhr.post = (url, body, onSuccess, onFail) =>
+  xhr({ method: 'POST', url, body, onSuccess, onFail });
+xhr.put = (url, body, onSuccess, onFail) =>
+  xhr({ method: 'PUT', url, body, onSuccess, onFail });
+
+xhr.delete = (url, onSuccess, onFail) =>
+  xhr({ method: 'DELETE', url, onSuccess, onFail });
+
+// xhr.get(
+//   'https://jsonplaceholder.typicode.com/users',
+//   (data) => {
+//     console.log(data);
+//   },
+//   (err) => {
+//     console.log(err);
+//   }
+// );
+
+/* -------------------------------------------------------------------------- */
+/*                               XHR Promise API                              */
+/* -------------------------------------------------------------------------- */
+
+const defaultOptions = {
+  method: 'GET',
+  url: '',
+  body: null,
+  errorMessage: '서버와의 통신이 원활하지 않습니다.',
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
   },
-  onFail({ message }) {
-    console.log(message);
-  },
-});
+};
+
+export function xhrPromise(options) {
+  const { method, url, body, headers, errorMessage } = {
+    ...defaultOptions,
+    ...options,
+    headers: {
+      ...defaultOptions.headers,
+      ...options.headers,
+    },
+  };
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.open(method, url);
+
+  Object.entries(headers).forEach(({ key, value }) => {
+    xhr.setRequestHeader(key, value);
+  });
+  xhr.send(JSON.stringify(body));
+
+  return new Promise((resolve, reject) => {
+    xhr.addEventListener('readystatechange', () => {
+      if (xhr.readyState === 4) {
+        if (xhr.status >= 200 && xhr.status < 400) {
+          resolve(JSON.parse(xhr.response));
+        } else {
+          reject({ message: errorMessage });
+        }
+      }
+    });
+  });
+}
+// xhrPromise({
+//   url: 'https://jsonplaceholder.typicode.com/users',
+// }).then((res) => {
+//   console.log(res);
+// });
+
+xhrPromise.get = (url) => {
+  return xhrPromise({ url });
+};
+xhrPromise.post = (url, body) => {
+  return xhrPromise({ url, body, method: 'POST' });
+};
+xhrPromise.put = (url, body) => {
+  return xhrPromise({ url, body, method: 'PUT' });
+};
+xhrPromise.delete = (url) => {
+  return xhrPromise({ url, method: 'DELETE' });
+};
